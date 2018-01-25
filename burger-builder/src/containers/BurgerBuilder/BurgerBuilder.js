b5dcdc70dafe5c9297b3b27fe08c267ba10acf45
badjok3 <dani.popoff@abv.bg>
2018-01-26 01:15:03 +0200

    minor updates to burger-builder

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            meat: 2,
-            cheese: 2
+            salad: 0,
+            bacon: 0,
+            meat: 0,
+            cheese: 0
         }
     }
     render() {
         return (
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls />
             </Auxiliary>
         )
     }
